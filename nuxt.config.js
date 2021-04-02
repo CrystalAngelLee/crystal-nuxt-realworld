5f0c2d9410afc2b77afa55f017b09483e9e99c56
CrystalAngelLee <crystaljiaxuan@163.com>
2021-04-02 23:45:42 +0800

    更新配置文件

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // 自定义路由规则
     extendRoutes(routes, resolve) {
       // 清除 Nuxt.js 基于 pages 目录默认生成的路由表规则
-      routes.splice(0);
+      routes.splice(0)
 
       routes.push(
         ...[
@@ -16,53 +16,53 @@ module.exports = {
               {
                 path: '', // 默认子路由
                 name: 'home',
-                component: resolve(__dirname, 'pages/home/'),
+                component: resolve(__dirname, 'pages/home/')
               },
               {
                 path: '/login',
                 name: 'login',
-                component: resolve(__dirname, 'pages/login/'),
+                component: resolve(__dirname, 'pages/login/')
               },
               {
                 path: '/register',
                 name: 'register',
-                component: resolve(__dirname, 'pages/login/'),
+                component: resolve(__dirname, 'pages/login/')
               },
               {
                 path: '/profile/:username',
                 name: 'profile',
-                component: resolve(__dirname, 'pages/profile/'),
+                component: resolve(__dirname, 'pages/profile/')
               },
               {
                 path: '/settings',
                 name: 'settings',
-                component: resolve(__dirname, 'pages/settings/'),
+                component: resolve(__dirname, 'pages/settings/')
               },
               {
                 path: '/editor',
                 name: 'create',
-                component: resolve(__dirname, 'pages/editor/'),
+                component: resolve(__dirname, 'pages/editor/')
               },
               {
                 path: '/editor/:slug',
                 name: 'editor',
-                component: resolve(__dirname, 'pages/editor/'),
+                component: resolve(__dirname, 'pages/editor/')
               },
               {
                 path: '/article/:slug',
                 name: 'article',
-                component: resolve(__dirname, 'pages/article/'),
-              },
-            ],
-          },
+                component: resolve(__dirname, 'pages/article/')
+              }
+            ]
+          }
         ]
-      );
-    },
+      )
+    }
   },
   // 注册插件
   plugins: ['~/plugins/request.js', '~/plugins/dayjs.js'],
   server: {
     port: 3306, // default: 3000
-    host: ‘0.0.0.0’, // default: localhost
-  },
-};
+    host: '0.0.0.0' // default: localhost
+  }
+}
